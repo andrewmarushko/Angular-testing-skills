2effcd29dc605f22e84d03d6130b47f791ac253b
Andrew Marushko <andrewmarushko@gmail.com>
2023-01-25 16:39:34 +0100

    latest

--- src/app/shooping-list/shooping-edit/shooping-edit.component.ts
+++ src/app/shooping-list/shooping-edit/shooping-edit.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shooping-edit',
   templateUrl: './shooping-edit.component.html',
-  styleUrls: ['./shooping-edit.component.scss']
+  styleUrls: ['./shooping-edit.component.scss'],
 })
 export class ShoopingEditComponent {
+  @ViewChild('nameInput', { static: false })
+  nameInputRef: ElementRef<HTMLInputElement> = {} as ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef<HTMLInputElement> =
+    {} as ElementRef;
 
+  constructor() {}
+
+  onAddItem() {}
 }
